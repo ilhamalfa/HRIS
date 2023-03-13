8993a120b8cf91a09d5c1a6f59c84dab39c9276d
candra211204 <candraputra21ch@gmail.com>
2023-03-13 13:39:05 +0700

    Commit pertama

---
+++ resources/views/dashboard/dashboard.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard</title>
+
+    {{-- Feather Icons --}}
+    <script src="https://unpkg.com/feather-icons"></script>
+</head>
+<body>
+    {{-- Navbar Start --}}
+    
+    {{-- Navbar End --}}
+    <nav>
+        <div class="nav-logo">
+            <a href="">KaryaOne</a>
+        </div>
+        <div class="nav-extra">
+            <a href=""></a>
+        </div>
+    </nav>
+
+    <script>
+        feather.replace()
+    </script>
+</body>
+</html>
