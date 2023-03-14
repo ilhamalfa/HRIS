033ed5c329ed9e967d8fa6695abb1d4a32009ebe
asaddin02 <114129501+asaddin02@users.noreply.github.com>
2023-03-14 14:22:35 +0700

    pull

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
@@ -13,7 +14,9 @@
     {{-- Google Fonts --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,700;1,300&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,700;1,300&display=swap"
+        rel="stylesheet">
 
     {{-- Feather Icons --}}
     <script src="https://unpkg.com/feather-icons"></script>
@@ -21,15 +24,21 @@
     {{-- CSS --}}
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
+
 <body>
-    
+    <button id="btn">sajkfus</button>
     <main>
         @yield('content')
     </main>
 
     {{-- Feather Icons --}}
+    <script src="{{ asset('jquery/jquery.min.js') }}"></script>
     <script>
         feather.replace()
+        $("#btn").click(function() {
+            alert("Handler for .click() called.");
+        });
     </script>
 </body>
+
 </html>
