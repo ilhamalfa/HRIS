71d71f77bcb77a3f80aa9c60f6d8bd7397a9382d
candra211204 <candraputra21ch@gmail.com>
2023-03-14 09:34:27 +0700

    .

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -73,5 +73,70 @@
             </div>
         </div>
     </div>
+</div>
+@endsection --}}
+
+@extends('auth.layout')
+
+@section('title', 'Register')
+
+@section('content')
+<a href="{{ url('/') }}">Home</a>
+<div class="signup-card">
+    <h1>Sign Up</h1>
+    <form action="{{ route('register') }}" method="POST">
+    @csrf
+        <div class="name">
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name" value="{{ old('name') }}" placeholder="ex. Name" required autocomplete="name" autofocus>
+        </div>    
+        <div class="email">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" value="{{ old('email') }}" placeholder="ex. email@g.c" required autocomplete="email">
+        </div>
+        <div class="password">
+            <label for="password">New Password</label>
+            <input type="password" id="password" name="password" placeholder="New Password" required autocomplete="new-password">
+        </div>
+        <div class="password">
+            <label for="password-confirm">Password Confirm</label>
+            <input type="password" id="password-confirm" name="password_confirmation" placeholder="Password Confirm" required autocomplete="new-password">
+        </div>
+        <div class="country">
+            <label for="country">Country</label>
+            <input type="text" id="country" name="negara" placeholder="Country" required>
+        </div>
+        <div class="province">
+            <label for="province">Province</label>
+            <input type="text" id="province" name="provinsi" placeholder="Province" required>
+        </div>
+        <div class="city">
+            <label for="city">City</label>
+            <input type="text" id="city" name="kota_asal" placeholder="City" required>
+        </div>
+        <div class="address">
+            <label for="address">Address</label>
+            <input type="text" id="address" name="alamat" placeholder="Address" required>
+        </div>
+        <div class="nik">
+            <label for="nik">NIK</label>
+            <input type="text" id="nik" name="nik" placeholder="NIK" required>
+        </div>
+        <div class="status">
+            <p>Select Your Status</p>
+            <select class="status-select" id="status" name="status">
+                <option value="lajang">Lajang</option>
+                <option value="menikah">User</option>
+                <option value="bercerai">User</option>
+            </select>
+        </div>
+        <div class="button">
+            <button type="submit">GET STARTED</button>
+        </div>
+    </form>
+</div>
+<div class="signin">
+    <p>Already have account?</p>
+    <a href="{{ route('login') }}">Sign In</a>
 </div>
 @endsection
