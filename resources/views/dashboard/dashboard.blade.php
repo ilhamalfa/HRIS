82b77afb8094c175f684ca319545ea6ee212a4f6
candra211204 <candraputra21ch@gmail.com>
2023-03-14 15:06:44 +0700

    ..

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <main>
         <div class="about-company">
-            <img src="{{ asset('dashboard/logo/manager.png') }}" alt=""></img>
+            <img src="{{ asset('logo/manager.png') }}" alt=""></img>
             <div class="description">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea nemo harum qui similique, aut pariatur nisi voluptatem! Quibusdam, quas? Ducimus officiis obcaecati est esse at suscipit, perspiciatis quis! Accusamus, reprehenderit?</p>
                 <a href="">GUIDE</a>
